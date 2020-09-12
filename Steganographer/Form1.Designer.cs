8a66ced80bef72cdc6cc89cab5963052e4593d85
egepaksoy <71194767+egepaksoy@users.noreply.github.com>
2020-09-13 00:31:21 +0300

    Add files via upload

---
+++ Steganographer/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Steganographer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.açToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.açToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.işlemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gizleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(406, 405);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(239, 438);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Metin";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.açToolStripMenuItem,
+            this.işlemToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(398, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // açToolStripMenuItem
+            // 
+            this.açToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.açToolStripMenuItem1});
+            this.açToolStripMenuItem.Name = "açToolStripMenuItem";
+            this.açToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.açToolStripMenuItem.Text = "Dosya";
+            // 
+            // açToolStripMenuItem1
+            // 
+            this.açToolStripMenuItem1.Name = "açToolStripMenuItem1";
+            this.açToolStripMenuItem1.Size = new System.Drawing.Size(88, 22);
+            this.açToolStripMenuItem1.Text = "Aç";
+            this.açToolStripMenuItem1.Click += new System.EventHandler(this.açToolStripMenuItem1_Click);
+            // 
+            // işlemToolStripMenuItem
+            // 
+            this.işlemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gizleToolStripMenuItem,
+            this.çıkartToolStripMenuItem});
+            this.işlemToolStripMenuItem.Name = "işlemToolStripMenuItem";
+            this.işlemToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.işlemToolStripMenuItem.Text = "İşlem";
+            // 
+            // gizleToolStripMenuItem
+            // 
+            this.gizleToolStripMenuItem.Name = "gizleToolStripMenuItem";
+            this.gizleToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.gizleToolStripMenuItem.Text = "Gizle";
+            this.gizleToolStripMenuItem.Click += new System.EventHandler(this.gizleToolStripMenuItem_Click);
+            // 
+            // çıkartToolStripMenuItem
+            // 
+            this.çıkartToolStripMenuItem.Name = "çıkartToolStripMenuItem";
+            this.çıkartToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.çıkartToolStripMenuItem.Text = "Çıkart";
+            this.çıkartToolStripMenuItem.Click += new System.EventHandler(this.çıkartToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 470);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(48, 438);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(136, 21);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Şifre";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 443);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Şifre :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 443);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Metin :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 499);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Steganographer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem açToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem açToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem işlemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gizleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkartToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Steganographer/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Steganographer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ac()
+        {
+            openFileDialog1.Filter = "Resim Dosyaları(*.png *.gif)|*.png;*.gif|All Files(*.*)|*.*";
+            openFileDialog1.DefaultExt = "resim";
+            openFileDialog1.ShowDialog();
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        public void kaydet()
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Png Resmi|*.png|Gif Resmi|*.gif";
+            save.DefaultExt = "resimout";
+
+            if (save.ShowDialog() == DialogResult.OK) //Dosya adı boş değilse kaydedecek.
+            {
+                // FileStream nesnesi ile kayıtı gerçekleştirecek.
+                FileStream DosyaAkisi = (FileStream)save.OpenFile();
+                switch (save.FilterIndex)
+                {
+                    case 1:
+                        pictureBox1.Image.Save(DosyaAkisi, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case 2:
+                        pictureBox1.Image.Save(DosyaAkisi, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case 3:
+                        pictureBox1.Image.Save(DosyaAkisi, System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                    case 4:
+                        pictureBox1.Image.Save(DosyaAkisi, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+
+                }
+                DosyaAkisi.Close();
+            }
+
+        }
+
+        public void gizle()
+        {
+            Bitmap img = new Bitmap(pictureBox1.Image) , resim2;
+            Color okunanRenk, rgb;
+            int r, g, b;
+            String mesaj = textBox1.Text;
+            int a = 0;
+            for (int x = 0; x < img.Width; x++)
+            {
+                for (int y= 0; y < img.Height; y++)
+                {
+                    okunanRenk = img.GetPixel(x, y);
+                    if (x < 1 && y < textBox1.TextLength)
+                    {
+                        Char harfler = Convert.ToChar(textBox1.Text.Substring(y, 1));
+                        int deger = Convert.ToInt32(harfler);
+
+                        r = okunanRenk.R;
+                        g = okunanRenk.G;
+                        b = deger;
+
+                        rgb = Color.FromArgb(r, g, b);
+                        img.SetPixel(x, y, rgb);
+                        if (a == 0)
+                        {
+                            if (MessageBox.Show("B nin pixel değeri: " + b + "\n devamını görmek istermisin", "Değer", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                            {
+                                a = 1;
+                            }
+                            else
+                            {
+                                a = 2;
+                            }
+                        }
+                        if (a == 1)
+                        {
+                            MessageBox.Show(textBox2.Text.Substring(y, 1) + "'nin pixel deği: " + b, "Değer");
+                        }
+
+                    }
+                    else
+                    {
+                        r = okunanRenk.R;
+                        g = okunanRenk.G;
+                        b = okunanRenk.B;
+
+                        rgb = Color.FromArgb(r, g, b);
+                        img.SetPixel(x, y, rgb);
+                    }
+                    if (x == img.Width -1 && y == img.Height - 1)
+                    {
+                        img.SetPixel(x, y, Color.FromArgb(okunanRenk.R, textBox2.TextLength, textBox1.TextLength));
+                    }
+                }
+                pictureBox1.Image = img;
+            }
+            decode(img);
+            kaydet();
+            MessageBox.Show("Metniniz başarıyla resme aktarıldı", "Başarılı");
+
+        }
+
+        public void cikart()
+        {
+            Bitmap resim = new Bitmap(pictureBox1.Image);
+            String yazı = "";
+            int R, G, B;
+            Color pixel;
+            Color sonPixel = resim.GetPixel(resim.Width - 1, resim.Height - 1);
+            int yazıUzunlugu = sonPixel.B;
+
+            for (int x = 0; x < resim.Width; x++)
+            {
+                for (int y = 0; y < resim.Height; y++)
+                {
+                    pixel = resim.GetPixel(x, y);
+
+                    if (x < 1 && y < yazıUzunlugu)
+                    {
+                        int deger = pixel.B;
+                        if (deger < 126)
+                        {
+
+                            char harf = Convert.ToChar(deger);
+                            string karakter = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(harf) });
+
+                            yazı += karakter;
+                        }
+                    }
+                }
+            }
+            
+            if (textBox2.Text == encode())
+            {
+                textBox1.Text = yazı;
+                MessageBox.Show("Metniniz başarıyla resimden çıkartıldı", "Başarılı");
+            }
+            else if (textBox2.Text == "")
+            {
+                MessageBox.Show("Şifre boş olmamalıdır", "Parola Hatası");
+            }
+            else
+            {
+                MessageBox.Show("Yanlış şifre", "Parola Hatası");
+            }
+            MessageBox.Show(encode());
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public void decode(Bitmap img)
+        {
+            int kontrol;
+            Color okunanRenk;
+            int r, g, b;
+            for (int x =0; x < img.Width;x++)
+            {
+                for (int y = 0; y < textBox2.TextLength; y++)
+                { 
+                    okunanRenk = img.GetPixel(x, y);
+                    if (x == 3 && y < textBox2.TextLength)
+                    {
+                        Color kontrolRenk = img.GetPixel(0,0);
+                        if (img.GetPixel(0,0).R == 1)
+                        {
+                            kontrol = 0;
+                            r = kontrol;
+                            g = kontrolRenk.G;
+                            b = kontrolRenk.B;
+                            img.SetPixel(0, 0, Color.FromArgb(r, g, b));
+                        }
+                        
+                        if (img.GetPixel(0, 0).R == 0)
+                        {
+                            kontrol = 1;
+                            r = kontrol;
+                            g = kontrolRenk.G;
+                            b = kontrolRenk.B;
+                            img.SetPixel(0, 0, Color.FromArgb(r, g, b));
+                        }
+                        
+                        Char harfler = Convert.ToChar(textBox2.Text.Substring(y, 1));
+                        int deger = Convert.ToInt32(harfler);
+
+                        r = okunanRenk.R;
+                        g = deger;
+                        b = okunanRenk.B;
+                        img.SetPixel(x, y, Color.FromArgb(r, g, b));
+
+                    }
+
+                    
+
+                }
+            }
+            pictureBox1.Image = img;
+        }
+
+
+        public string encode()
+        {
+            Color okunanRenk;
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            String sifre = "";
+            int sifreUzunlugu = img.GetPixel(img.Width - 1, img.Height - 1).G;
+
+            for (int x = 0; x < img.Width; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    okunanRenk = img.GetPixel(x, y);
+                    if (x == 3 && y < sifreUzunlugu)
+                    {
+                        int deger = okunanRenk.G;
+
+                        char harf = Convert.ToChar(deger);
+
+                        string karakter = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(harf) });
+
+                        sifre += karakter;
+                    }
+                }
+            }
+            return sifre;
+        }
+
+
+
+
+
+
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gizle();
+            textBox1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cikart();
+        }
+
+        private void açToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ac();
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void gizleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Gizle";
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void çıkartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Çıkart";
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Önce Bir Resim Açılamlıdır", "Hata");
+                ac();
+            }
+            else
+            {
+                if(button1.Text == "")
+                {
+                    MessageBox.Show("İşlemi seçin");
+                }
+
+                if (button1.Text == "Gizle")
+                {
+                    gizle();
+                    textBox1.Text = "";
+                }
+                if (button1.Text == "Çıkart")
+                {
+                    cikart();
+                }
+            }
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
